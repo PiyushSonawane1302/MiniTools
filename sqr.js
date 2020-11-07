cf4dff11af121cc9b2a75d95d1b558e0637922b1
PiyushSonawane1302 <piyushsonawane4455@gmail.com>
2020-11-07 10:41:00 +0530

    sqr finder added

---
+++ sqr.js
@@ -0,0 +1,44 @@
+const myForm=document.querySelector('#my-form');
+const msg=document.querySelector('.msg');
+const num=document.querySelector('#Enter-num');
+const sqr=document.querySelector('#sqr');
+
+
+
+// document.getElementById("per").addEventListener("mouseover", mouseOver);
+// document.getElementById("per").addEventListener("mouseout", mouseOut);
+
+// function mouseOver() {
+//     document.getElementById("per").innerHTML="check your answer here ";
+//   }
+
+// function mouseOut() {
+//     document.getElementById("per").innerHTML="Answer";
+//   } 
+
+
+
+
+myForm.addEventListener('submit',onSquare);
+
+function onSquare(e)
+{
+  e.preventDefault();
+    console.log(num.value);
+    if(num.value==="")
+    {
+        msg.classList.add('error')
+        msg.innerHTML="Please enter the Number !";
+
+        setTimeout(()=>msg.remove(),4000);
+    }
+
+  else{
+  let x=num.value;
+  let y=x*x;
+  document.getElementById('sqr').innerHTML=`Square of ${num.value} is = ${y.toPrecision(4)}  `;
+}
+}
+
+
+
